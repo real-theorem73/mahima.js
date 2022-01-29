1e165c7b3ad0a0239d556e461779a7d7479e5303
real-theorem73 <98615193+real-theorem73@users.noreply.github.com>
2022-01-29 15:25:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+document.write('<b>hello mahima</b>');
+document.write('<br>');
+
+var i;
+var j;
+for (i = 1; i <= 8; i++)
+{
+  for(j=1;j<=i;j++)
+{
+  document.write('*')
+}
+document.write('<br>');
+  
+}
+
+var a=prompt('enter value of a');
+var b=prompt('enter value of b');
+var A=parseInt(a);
+var B=parseInt(b);
+A=A+B;
+B=A-B;
+A=A-B;
+document.write(A);
+document.write('<br>');
+document.write(B);
+
+
+var i=20;
+var j=18;
+var k=i+j;
+document.write('<br>');
+document.write(k);
+
+document.write('<br>')
+document.write('good morning');
